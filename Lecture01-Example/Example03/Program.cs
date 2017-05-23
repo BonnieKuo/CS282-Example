08da89a341c6a5e009c9025ddc66789f5b3a265c
FrankLikuohao <frank.likuohao@gmail.com>
2017-05-23 22:21:36 +0800

    Example3

--- Lecture01-Example/Example03/Program.cs
+++ Lecture01-Example/Example03/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Example02
+namespace Example03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("你的姓名:{0}", Console.ReadLine());
+            Student someone = new Student();
+            someone.name = "QQ";
+            Console.WriteLine("你的姓名:{0}", someone.name);
         }
     }
 }
